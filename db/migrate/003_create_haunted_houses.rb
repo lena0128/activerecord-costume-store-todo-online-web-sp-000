5b530fb3aafbf1c538f9df4ea2d26af8eabfc450
Lena Hou <lenahou2013@gmail.com>
2021-05-23 18:21:40 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,7 +7,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :location
       t.string :theme
       t.float :price
-      t.boolean :faimly_friendly
+      t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_time
       t.text :long_description
